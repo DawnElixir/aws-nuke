aa6a4287216e19d108aec2853b32f7ea6bc58848
DawnElixir <hlhmover@gmail.com>
2023-05-05 16:43:30 +0800

    Update elasticache-replicationgroups.go
    fix bug for replicationgroups

--- resources/elasticache-replicationgroups.go
+++ resources/elasticache-replicationgroups.go
@@ -1,19 +1,14 @@
 package resources
 
 import (
-	"fmt"
-	
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/elasticache"
-	"github.com/aws/aws-sdk-go/service/sts"
-	"github.com/rebuy-de/aws-nuke/v2/pkg/types"
 )
 
 type ElasticacheReplicationGroup struct {
 	svc     *elasticache.ElastiCache
 	groupID *string
-	tags      []*elasticache.Tag
 }
 
 func init() {
@@ -22,14 +17,6 @@ func init() {
 
 func ListElasticacheReplicationGroups(sess *session.Session) ([]Resource, error) {
 	svc := elasticache.New(sess)
-        // Lookup current account ID
-	stsSvc := sts.New(sess)
-	callerID, err := stsSvc.GetCallerIdentity(&sts.GetCallerIdentityInput{})
-	if err != nil {
-		return nil, err
-	}
-	accountID := callerID.Account
-	region := svc.Config.Region
 	var resources []Resource
 
 	params := &elasticache.DescribeReplicationGroupsInput{MaxRecords: aws.Int64(100)}
@@ -41,17 +28,9 @@ func ListElasticacheReplicationGroups(sess *session.Session) ([]Resource, error)
 		}
 
 		for _, replicationGroup := range resp.ReplicationGroups {
-			// Arn creation for listing tags
-		        tags, err := svc.ListTagsForResource(&elasticache.ListTagsForResourceInput{
-			    ResourceName: aws.String(fmt.Sprintf("arn:aws-cn:elasticache:%s:%s:cluster:%s", *region, *accountID, *cacheCluster.CacheClusterId)),
-		        })
-		        if err != nil {
-			    continue
-		        }
 			resources = append(resources, &ElasticacheReplicationGroup{
 				svc:     svc,
 				groupID: replicationGroup.ReplicationGroupId,
-				tags:      tags.TagList,
 			})
 		}
 
@@ -80,14 +59,4 @@ func (i *ElasticacheReplicationGroup) Remove() error {
 
 func (i *ElasticacheReplicationGroup) String() string {
 	return *i.groupID
-}
-func (i *ElasticacheCacheCluster) Properties() types.Properties {
-	properties := types.NewProperties()
-	properties.Set("Identifier", i.clusterID)
-
-	for _, tag := range i.tags {
-		properties.SetTag(tag.Key, tag.Value)
-	}
-
-	return properties
 }
